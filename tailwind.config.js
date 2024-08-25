ade93e0915faef09ace433b78913a26c8c06d7fe
pranto113015 <pranto3876@gmail.com>
2024-08-25 12:03:48 +0600

    add home hero section
    1. add hero section image
    
    2. add hero content
    
    3. add animation rotate custom css
    
    4. add blob css custom shadow
    
    5. add socical icon inside hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,26 @@ module.exports = {
       xl: "1180px"
     },
     extend: {},
+    keyframes: {
+      move: {
+        "50%": { transform: "translateY(-1rem)" }
+      },
+      rotate: {
+        "0%": { transform: "rotate(0deg)" },
+        "100%": { transform: "rotate(360deg)" },
+      },
+      scaleUp: {
+        "0%": { transform: "scale(0.8)" },
+        "50%": { transform: "scale(1.2)" },
+        "100%": { transform: "scale(0.8)" },
+      }
+    },
+    animation: {
+      movingY: "move 3s linear infinite",
+      rotating: "rotate 15s linear infinite",
+      scalingUp: "scaleUp 3s linear infinite",
+
+    },
     // custom font family
     fontFamily: {
       Jost: ["Jost", "sans-serif"],

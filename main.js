72db9b00835e757eac58bcd77cc8b0f99a1254e4
pranto113015 <pranto3876@gmail.com>
2024-08-27 12:22:08 +0600

    add change background header at main.js file

--- main.js
+++ main.js
@@ -33,6 +33,20 @@ window.addEventListener("scroll", scrollup)
 
 
 /*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
+const scrollHeader = () => {
+    const header = document.getElementById("navbar")
+    if (this.scrollY >= 50) {
+        header.classList.add("border-b", "border-yellow-500")
+
+    } else {
+        header.classList.add("-bottom-1/2")
+        header.classList.remove("border-b", "border-yellow-500")
+    }
+}
+window.addEventListener("scroll", scrollHeader)
+
+
+
 
 
 /*~~~~~~~~~~~~~~~ SWIPER ~~~~~~~~~~~~~~~*/
